6fde485f0fcaa7d66f76b732cff26e8719a785a6
Edu <eduardobrunner94@gmil.com>
2022-08-05 20:08:53 -0300

    agrego metodos get y post y ejecuto el servidor para probar

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print ("Hello World")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+posts = []
+
+@app.get('/')
+def read_root():
+    return{"welcome": "welcome to my rest API"}
+
+@app.get('/posts')
+def get_posts():
+    return posts
+
+@app.post('/posts') #.post es el metodo http y /posts es la ruta del host
+def save_post():
+    print(post)
+    return "received"
